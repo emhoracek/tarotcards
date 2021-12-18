65ad7f0199ae59fd07a64cd1c5a03ca4b741b118
Libby Horacek <libby@daydrea.me>
2021-12-18 17:34:30 -0500

    Add divinatory text

--- script.js
+++ script.js
@@ -5,50 +5,114 @@ let currentCard = 0
 
 document.addEventListener("DOMContentLoaded", function(event) {
   shuffleArray(cards);
-    
+
   mkTableCards();
-  
-  const explainer = document.getElementById("")
+
+  const openExplainer = document.getElementById("open-explainer")
+  const closeExplainer = document.getElementsByClassName("explainer-button")[0]
+  closeExplainer.addEventListener('click', e => {
+    const explainer = document.getElementById("explainer")
+    explainer.style.display = "none"
+    openExplainer.style.display = "block"
+  })
+  openExplainer.addEventListener('click', e => {
+    const explainer = document.getElementById("explainer")
+    explainer.style.display = "block"
+    openExplainer.style.display = "none"
+  })
+
+
   const explainerOpt1 = document.getElementById("cs")
   const explainerOpt2 = document.getElementById("waite")
- /*
-  var rad = document.getElementsByTagName("input")
-  var prev = null;
-  for (var i = 0; i < rad.length; i++) {
-      rad[i].addEventListener('change', function() {
-          //const selected = document.querySelector('input[name="interpretation"]:checked').value;
-          if (selected.id == rad[i].id) { 
-          interpretation
-          } 
-          (prev) ? console.log(prev.value): null;
-          if (this !== prev) {
-              prev = this;
+
+  const inputs = document.getElementsByTagName("input");
+  for (var i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener('change', e => {
+          const selected = document.querySelector('input[name="interpretation"]:checked');
+          if (selected.id == e.target.id) {
+            displayExplanation(e.target.value)
           }
-          console.log(this.value)
       });
-  }*/
-  
+  }
+
   const table = document.getElementById("table")
 
   initializeDeck(table);
 });
 
+function displayExplanation(interpretation) {
+  const selectedCardNode = document.getElementsByClassName("selected")[0]
+  if (selectedCardNode) {
+    const selectedCard = tableCards[selectedCardNode.parentElement.id]
+
+    if (selectedCard && selectedCard.visible == "front") {
+      const placeholder = document.getElementsByClassName("explanation-placeholder")[0]
+      placeholder.style.display = "none"
+
+      const map = {
+        "cs": {
+          positiveTitle: "Positive",
+          negativeTitle: "Negative",
+          positive: selectedCard["cspositive"],
+          negative: selectedCard["csnegative"]
+        },
+        "waite": {
+          positiveTitle: "Upright",
+          negativeTitle: "Reversed",
+          positive: selectedCard["waiteregular"],
+          negative: selectedCard["waitereversed"]
+        },
+      }
+
+      let main = document.querySelector(".explanation-main")
+      main.style.display = "block"
+
+      let main = document.querySelector(".explanation-main")
+      main.style.display = "block"
+
+      let positiveh = document.querySelector(".explanation-positive h1")
+      positiveh.innerText = map[interpretation].positiveTitle
+      let negativeh = document.querySelector(".explanation-negative h1")
+      negativeh.innerText = map[interpretation].negativeTitle
+
+      let positivep = document.querySelector(".explanation-positive p")
+      positivep.innerText = map[interpretation].positive
+      let negativep = document.querySelector(".explanation-negative p")
+      negativep.innerText = map[interpretation].negative
+    } else {
+      const placeholder = document.getElementsByClassName("explanation-placeholder")[0]
+      placeholder.style.display = "block"
+
+      let main = document.querySelector(".explanation-main")
+      main.style.display = "none"
+    }
+  } else {
+    const placeholder = document.getElementsByClassName("explanation-placeholder")[0]
+    placeholder.style.display = "block"
+
+    let main = document.querySelector(".explanation-main")
+    main.style.display = "none"
+  }
+}
+
+
 function mkTableCards () {
   cards.forEach((c, i) => {
     const cardId = c["label"].toLowerCase().replace(/[^a-z]/g, "")
     const orientation = [0, 180][Math.floor(Math.random() * 2)];
-    
-    tableCards[cardId] = { 
-      "id": cardId, 
-      "x": 0, "y": 0, 
-      "orientation": orientation, 
+
+    tableCards[cardId] = {
+      "id": cardId,
+      "x": 0, "y": 0,
+      "title": c.label,
+      "orientation": orientation,
       "image": c.image,
-      "visible": "back", 
+      "visible": "back",
       "drawn": false,
       "cspositive": c.cspositive,
       "csnegative": c.csnegative,
       "waiteregular": c.waiteregular,
-      "waitereversed": c.waitereversed } 
+      "waitereversed": c.waitereversed }
   })
 }
 
@@ -56,31 +120,32 @@ function moveToTop(cardId) {
   var table = document.getElementById("table");
   var cards = document.getElementsByClassName("card");
   var card = document.getElementById(cardId);
-  
-  console.log("card", cardId, card)
+
   if (cardId == "table") { return false }
-  
+
   if (card.parentElement.id == "camera") { return false }
-  
+
   // clear selected from all children
   for (let i = 0; i < cards.length; i++) {
     cards[i].children[0].className = "innerCard"
   }
-  
+
   // remove card from table
   for (let i = 0; i < cards.length; i++) {
     const thisCardId = cards[i].id
-    
+
     if (thisCardId == cardId) {
-      console.log("removed", thisCardId)
       table.removeChild(cards[i]);
     }
   }
 
   // select card and add it back to the table
-  console.log("parent id:", card.parentElement ? card.parentElement.id: "no parent")
   card.children[0].className = "innerCard selected"
   table.appendChild(card);
+
+  // display interpretation of card
+  const selected = document.querySelector('input[name="interpretation"]:checked');
+  displayExplanation(selected.value)
 }
 
 function shuffleArray(array) {
@@ -101,14 +166,13 @@ function createCard(card) {
   innerDiv.className = "innerCard"
   innerDiv.style.transform = toRotate(deckCard.orientation)
   cardNode.appendChild(innerDiv)
-  
+
   innerDiv.appendChild(mkRotateLeft(cardId))
   innerDiv.appendChild(mkRotateRight(cardId))
   innerDiv.appendChild(mkFlip(cardId))
-  
+
   innerDiv.addEventListener("click", e => { e.preventDefault()
     let cardId = e.target.parentElement.id
-    //console.log("click target", e.target.id, e.target.parentElement.id)
     if (cardId == "table") {
       cardId = e.target.id
     }
@@ -117,7 +181,7 @@ function createCard(card) {
     return false;
   })
 
-  
+
   tableCards[cardId].drawn = true
   tableCards[cardId].node = cardNode
   return cardNode
@@ -129,7 +193,7 @@ function mkFlip(cardId) {
   div.className = "button flip"
   const textnode = document.createTextNode("↑");
   div.appendChild(textnode)
-  
+
   div.addEventListener("click", e => {
     e.preventDefault();
     const cardNode = e.target.parentElement
@@ -139,6 +203,9 @@ function mkFlip(cardId) {
       cardNode.style["background-image"] = "url('" + tableCards[cardId].image + "')"
       cardNode.style["background-size"] = "185px"
       e.target.innerText= "↓"
+
+      const selected = document.querySelector('input[name="interpretation"]:checked');
+      displayExplanation(selected.value)
     } else {
       tableCards[cardId].visible = "back"
       cardNode.style["background-color"] = ""
@@ -156,14 +223,14 @@ function mkRotateLeft(cardId) {
   div.className = "button rotateLeft"
   const textnode = document.createTextNode("←");
   div.appendChild(textnode)
-  
+
   div.addEventListener("click", e => {
     e.preventDefault();
-    
+
     tableCards[cardId].orientation = rotateLeft(tableCards[cardId].orientation)
     tableCards[cardId].node.style.transform = toTransform(tableCards[cardId])
     tableCards[cardId].node.children[0].style.transform = toRotate(tableCards[cardId].orientation)
-    
+
     return false;
   })
   return div
@@ -171,7 +238,6 @@ function mkRotateLeft(cardId) {
 
 function shadow (orientation) {
   const actualOrientation = Math.abs(orientation % 360);
-  console.log("or", actualOrientation)
   if (actualOrientation == 0) {
     return "10px 5px 10px #666666"
   }
@@ -202,15 +268,15 @@ function mkRotateRight(cardId) {
   div.className = "button rotateRight"
   const textnode = document.createTextNode("→");
   div.appendChild(textnode)
-  
+
   div.addEventListener("click", e => {
     e.preventDefault();
     const orientation = rotateRight(tableCards[cardId].orientation)
     tableCards[cardId].orientation = orientation
-    
+
     tableCards[cardId].node.style.transform = toTransform(tableCards[cardId], orientation.deg)
     tableCards[cardId].node.children[0].style.transform = toRotate(orientation)
-    
+
     return false;
   })
   return div
@@ -255,9 +321,8 @@ interact('.draggable').draggable({
         const scrollX = window.scrollX
         card.y += scrollY
         card.x += scrollX
-        
+
         if (camera.children.length < 2) {
-          console.log("more")
           if (currentCard <= cards.length) {
             const newCard = createCard(cards[currentCard])
             currentCard = currentCard + 1
@@ -273,21 +338,20 @@ interact('.draggable').draggable({
     move (event) {
       const targetId = event.target.id
       let card = tableCards[targetId]
-      
+
       const scrollY = window.scrollY
-      
+
       card.x += event.dx
       card.y += event.dy // add scroll distance to this!!
-      
+
       tableCards[targetId].x = card.x
-      tableCards[targetId].y = card.y 
-      
+      tableCards[targetId].y = card.y
+
       event.target.style.transform = toTransform(card)
     },
     end (event) {
       const classes = event.target.className
       const newClasses = classes.split(" ").filter(c => c !== "floating").join(" ")
-      console.log("classes",classes, newClasses)
       event.target.className = newClasses
       event.target.children[0].style["box-shadow"] = "none"
     },
